109a8a390de9cd994510f0770bb6d4d0e8640b75
beny marel PAMBOU <marel@ben0ptimus.org>
2014-07-03 10:25:46 +0000

    change py template to my convenience

--- templates/=template=*.py
+++ templates/=template=*.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-# vim:fenc=utf-8
-#
 # Copyright © %YEAR% %USER% <%MAIL%>
-#
+
+__author__  = "%USER% ♟ <%MAIL%>"
+__version__ = "%HERE%"
+__date__    = ""
+
 # Distributed under terms of the %LICENSE% license.
 
 """
